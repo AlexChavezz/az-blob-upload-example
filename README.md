d16c7b05ccb5a54b8a422166b0ed074b0c759f52
AlexChavezz <alexischavez1234.ac@gmail.com>
2023-02-02 18:48:23 -0600

    upload-txt-files implemented

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-### ABOUT THIS PROYECT
-## This is an application buided using node js.
-## The propouse of this is simply upload files to an azure bob storage.
+## ABOUT THIS PROYECT
+This is an application buided using node js.
+The propouse of this is simply upload files to an azure bob storage.
--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 const { BlobServiceClient } = require('@azure/storage-blob');
 
 
+server.use(app.json());
+
+
 /* 
     FUNCTION USED TO CREATE A BLOB CONTAINER
     Should be resive a param called name to set this value as a container name
@@ -15,16 +18,59 @@ const { BlobServiceClient } = require('@azure/storage-blob');
 
 server.get('/create/:name', async function (req, res) {
     const { name } = req.params;
-    const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-    if (!accountName) throw new Error('Azure storage account name is not defined');
 
-    const blobServiceClient = BlobServiceClient.fromConnectionString(
-        process.env.AZURE_STORAGE_CONNECTION_STRING
-    );
+    try {
+        // -> Get azure storage account name from environment variables
+        const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 
-    const containerClient = blobServiceClient.getContainerClient(name);
-    const createContainerResponse = await containerClient.create();
-    res.send(`Container was created successfully.\n\trequestId:${createContainerResponse.requestId}\n\tURL: ${containerClient.url}`);
+        // -> Check if account name variable exists
+        if (!accountName) {
+            // throw new Error('Azure storage account name is not defined');
+            return res.status(500).json({ message: "INTERNAL SERVER ERROR" })
+        }
+
+        // -> Define a client reference to bob storage service
+        const blobServiceClient = BlobServiceClient.fromConnectionString(
+            process.env.AZURE_STORAGE_CONNECTION_STRING
+        );
+
+        const containerClient = blobServiceClient.getContainerClient(name);
+        const createContainerResponse = await containerClient.create();
+        if (createContainerResponse) {
+            return res.status(201).json({ message: "Container was created successfully" })
+        }
+    }
+    catch (error) {
+        console.error(`Error: ${error}`)
+        return res.status(500).json({ message: "INTERNAL SERVER ERROR" })
+    }
+})
+
+/*
+    FUNCTION TO UPLOAD A FILE TO A BLOB CONTAINER
+    This file must be a .txt archive
+*/
+
+server.post('/upload', async function (req, res) {
+
+    // -> Get text content and file name from request body
+    const { text, fileName } = req.body;
+
+    try {
+        // -> Upload Text First
+        const blobServiceClient = BlobServiceClient.fromConnectionString(
+            process.env.AZURE_STORAGE_CONNECTION_STRING
+        );
+
+        const containerClient = blobServiceClient.getContainerClient("text-uploads-example");
+        const blobBlockClient = containerClient.getBlockBlobClient(`${fileName}.txt`);
+
+        await blobBlockClient.upload(text, text.length);
+        return res.status(201).json({message:"File was uploaded successfully"});
+    }
+    catch (error) {
+        return res.status(500).json({ message: "INTERNAL SERVER ERROR" })
+    }
 })
 
 
